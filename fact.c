8b5f2c2e5fe96963ab4d9abe3c24987b3a10b53b
rajrkn973 <83324431+rajrkn973@users.noreply.github.com>
2021-04-28 16:49:50 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int i,f=1,n'
+    printf("enter a number");
+  scanf("%d",n);
+  for(i=1;i<=n;i++)
+    f=f*i;
+}
+printf("factorial of %d is :%d",f,n);
+return 0;
+}
