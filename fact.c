0e45b5aab03856d7efc9c78165bae1ca0e109c6f
rajrkn973 <83324431+rajrkn973@users.noreply.github.com>
2024-03-28 09:39:50 +0530

    fact.c

--- fact.c
+++ fact.c
@@ -2,11 +2,11 @@
 int main()
 {
   int i,f=1,n;
-    printf("enter a number");
-  scanf("%d",n);
-  for(i=1;i<=n;i++)
-    f=f*i;
-}
-printf("factorial of %d is :%d",f);
+    printf("enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        f=f*i;
+    }
+printf("factorial of %d is :%d",n,f);
 return 0;
 }
